a4da8294487112da0e8f1c6d4d0f477f6a456863
serbaev_ilnur <serbaevilnur@gmail.com>
2019-08-01 10:28:33 +0500

    ch1

--- greenProject/src/main/scala/ch1/impl.scala
+++ greenProject/src/main/scala/ch1/impl.scala
@@ -15,4 +15,18 @@ object impl {
     override def format(a: Cat): String =
       s"${Printable.format(a.name)} is a ${Printable.format(a.age)} year-old ${Printable.format(a.color)} cat."
   }
+
+  import cats.Show
+  import cats.instances.int._
+  import cats.instances.string._
+  import cats.syntax.show._
+  implicit val catShow: Show[Cat] =
+    Show.show(cat => s"${cat.name.show} is a ${cat.age.show} year-old ${cat.color.show} cat.")
+
+  import cats.Eq
+  import cats.instances.int._
+  import cats.instances.string._
+  import cats.syntax.eq._
+  implicit val catEq: Eq[Cat] =
+    Eq.instance[Cat]((c1,c2) => c1.name === c2.name && c1.age === c2.age && c1.color === c2.color)
 }
--- greenProject/src/main/scala/ch1/main.scala
+++ greenProject/src/main/scala/ch1/main.scala
@@ -9,6 +9,40 @@ object main extends App {
   val cat = Cat("c", 1, "red")
 
   print(cat)
-
   cat.print
+
+  import cats.Show
+  import cats.instances.int._
+  import cats.instances.string._
+  import cats.syntax.show._
+  import impl.catShow
+
+  val showInt: Show[Int] = Show.apply[Int]
+  val showString: Show[String] = Show.apply[String]
+
+  showInt.show(123)
+  123.show
+
+  println(cat.show)
+
+
+  import cats.Eq
+  import cats.instances.int._
+  import cats.instances.option._
+  import cats.syntax.eq._
+
+  val eqInt = Eq[Int]
+  eqInt.eqv(123,123)
+  eqInt.eqv(123,124)
+
+  123 === 123
+  123 =!= 124
+
+  Option(1) =!= Option.empty[Int]
+
+  import impl.catEq
+  val cat2 = Cat("c2", 2, "black")
+  cat =!= cat2
+  Option(cat) =!= Option.empty[Cat]
+  Option(cat) =!= Option(cat2)
 }
