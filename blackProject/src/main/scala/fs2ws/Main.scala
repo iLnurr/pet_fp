f35aa9e4a2641675a7cc26d8125fa3e72e0b5991
serbaev_ilnur <serbaevilnur@gmail.com>
2019-08-15 20:52:21 +0500

    fs2ws: use algebra

--- blackProject/src/main/scala/fs2ws/Main.scala
+++ blackProject/src/main/scala/fs2ws/Main.scala
@@ -1,12 +1,14 @@
 package fs2ws
 
 import cats.effect.{ConcurrentEffect, ExitCode, IO, IOApp}
+import JsonSerDe._
 import cats.syntax.flatMap._
 
 object Main extends IOApp {
   implicit val ce: ConcurrentEffect[IO] = IO.ioConcurrentEffect
+  implicit val mp = new MessageProcessorAlgebraImpl()
   override def run(args: List[String]): IO[ExitCode] = {
-    (FS2Server.start[IO](identity).compile.drain
+    (new FS2ServerImpl().startWS(9000)
       >> IO.pure(ExitCode.Success))
       .handleErrorWith(ex =>
         IO {
