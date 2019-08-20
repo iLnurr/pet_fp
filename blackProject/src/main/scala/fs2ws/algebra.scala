1dd6a53828fd6b99de4fd86f07530b07057e37ae
serbaev_ilnur <serbaevilnur@gmail.com>
2019-08-20 18:06:18 +0500

    fs2ws: algebra updated

--- blackProject/src/main/scala/fs2ws/algebra.scala
+++ blackProject/src/main/scala/fs2ws/algebra.scala
@@ -1,5 +1,7 @@
 package fs2ws
 
+import java.util.UUID
+
 import cats.tagless.finalAlg
 import fs2ws.Domain.Message
 import fs2ws.impl.State._
@@ -16,6 +18,7 @@ trait JsonDecoder[F[_], A] {
 
 @finalAlg
 trait Clients[F[_]] {
+  def get(id: UUID): F[Option[Client[F]]]
   def register(c: Client[F]): F[Client[F]]
   def unregister(c: Client[F]): F[Unit]
   def broadcast(message: Message, filterF: Client[F] => Boolean): F[Unit]
