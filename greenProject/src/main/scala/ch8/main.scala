002a0e2e6a4304fb1abc8c36c5497e9845dc225f
serbaev_ilnur <serbaevilnur@gmail.com>
2019-09-04 11:34:40 +0500

    cats - refac

--- greenProject/src/main/scala/ch8/main.scala
+++ greenProject/src/main/scala/ch8/main.scala
@@ -3,10 +3,10 @@ package ch8
 import cats.Id
 
 object main extends App {
-  def testTotalUptime() = {
+  def testTotalUptime(): Unit = {
     val hosts = Map("host1" -> 10, "host2" -> 6)
     val testClient = new TestUptimeClient(hosts)
-    val service = new UptimeService {
+    val service: UptimeService[Id] = new UptimeService {
       val client: UptimeClient[Id] = testClient
     }
     val actual = service.getTotalUptime(hosts.keys.toList)
