1ebdc8ca7e04968133fc505eecc4934ca2ef91cc
serbaev_ilnur <serbaevilnur@gmail.com>
2019-09-04 14:54:42 +0500

    fs2ws - main

--- blackProject/src/main/scala/fs2ws/Main.scala
+++ blackProject/src/main/scala/fs2ws/Main.scala
@@ -2,16 +2,18 @@ package fs2ws
 
 import cats.effect.{ConcurrentEffect, ExitCode, IO, IOApp}
 import fs2ws.impl.JsonSerDe._
-import cats.syntax.flatMap._
 import fs2ws.impl.ServerImpl
 import fs2ws.impl.State.ConnectedClients
 
 object Main extends IOApp {
   implicit val ce: ConcurrentEffect[IO] = IO.ioConcurrentEffect
   override def run(args: List[String]): IO[ExitCode] = {
-    val clients = ConnectedClients.create[IO].unsafeRunSync()
-    (new ServerImpl(clients, startMsgStream(encoder.toJson, incomingMessageDecoder.fromJson, _)).start()
-      >> IO.pure(ExitCode.Success))
+    (for {
+      clients <- ConnectedClients.create[IO]
+      _ <- new ServerImpl(clients, startMsgStream(encoder.toJson, incomingMessageDecoder.fromJson, _)).start()
+    } yield {
+      ExitCode.Success
+    })
       .handleErrorWith(ex =>
         IO {
           println(s"Server stopped with error ${ex.getLocalizedMessage}")
