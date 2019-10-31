93b2d0b24f2063de3e1277684fd47deb4ec5b0b7
serbaev_ilnur <serbaevilnur@gmail.com>
2019-10-31 14:53:03 +0500

    red - free
    ClientAlg, ConnectionAlg, ServerAlg, Converter

--- redProject/src/main/scala/com/iserba/fp/Main.scala
+++ redProject/src/main/scala/com/iserba/fp/Main.scala
@@ -2,28 +2,20 @@ package com.iserba.fp
 
 import java.util.UUID
 
-import com.iserba.fp.free.algebra._
 import com.iserba.fp.free.compilers._
-import com.iserba.fp.model.{Event, Model, Request, Response}
-import com.iserba.fp.utils.Free.freeMonad
-import com.iserba.fp.utils.{Free, Monad}
+import com.iserba.fp.free.impl
+import com.iserba.fp.model._
 
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 
 object Main extends App {
-  val dummyLogic: Converter[Request, Response] = new Converter[Request, Response] {
-    def convert: Request => Response = req => Response(req.entity.map(ev => ev.copy(ts = ev.ts * 2)))
-  }
   def checkResponse(response: Response, request: Request): Unit =
-    assert(response == dummyLogic.convert(request))
+    assert(response == impl.dummyLogic.convert(request))
 
   def checkFree(request: Request): Unit = {
-    implicit val connFreeMonad: Monad[({type f[a] = Free[ConnectionAlg, a]})#f] =
-      freeMonad[ConnectionAlg]
-
-    val (serverId, channel) = Await.result(runServerFree(dummyLogic), Duration.Inf)
+    val (serverId, channel) = Await.result(runServerFree(), Duration.Inf)
     implicit val pool: Map[UUID, RequestResponseChannel] = Map(serverId -> channel)
 
     val checkResult = runClientFree(serverId).flatMap(makeRequest(request, _))
--- redProject/src/main/scala/com/iserba/fp/free/algebra.scala
+++ redProject/src/main/scala/com/iserba/fp/free/algebra.scala
@@ -3,21 +3,12 @@ package com.iserba.fp.free
 import java.util.UUID
 
 import com.iserba.fp.free.algebra._
-import com.iserba.fp.free.interpreters.{
-  clientToConnectionInterpreter,
-  connectionToFutureInterpreter,
-  serverToConnectionInterpreter
-}
+import com.iserba.fp.free.interpreters._
 import com.iserba.fp.model._
-import com.iserba.fp.utils.Free.{Translate, ~>}
+import com.iserba.fp.utils.Free.{Translate, freeMonad, ~>}
 import com.iserba.fp.utils.StreamProcessHelper._
 import com.iserba.fp.utils.{Free, Monad}
-import com.iserba.fp.{
-  IO,
-  RequestResponseChannel,
-  ResponseStreamByRequest,
-  model
-}
+import com.iserba.fp.{IO, RequestResponseChannel, ResponseStreamByRequest, model}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.language.{higherKinds, implicitConversions}
@@ -28,22 +19,28 @@ object algebra {
   }
 
   sealed trait ServerAlg[T]
-  case class RunServer(converter: Converter[Request,Response]) extends ServerAlg[(UUID, RequestResponseChannel)]
-  type Server[T] = Free[ServerAlg, T]
-  def runServer(converter: Converter[Request,Response]): Server[(UUID, RequestResponseChannel)] =
-    Free.liftF(RunServer(converter))
   def createServerChannel(converter: Converter[Request,Response]): RequestResponseChannel =
     constantF[IO, ResponseStreamByRequest](req =>
       emit(converter.convert(req))
     )
+  case class RunServer() extends ServerAlg[ServerChannel]
+  case class ServerChannel(id: UUID, channel: RequestResponseChannel) extends ServerAlg[(UUID, RequestResponseChannel)]
+  type Server[T] = Free[ServerAlg, T]
+  def runServer(): Server[ServerChannel] =
+    Free.liftF(RunServer())
+  def serverChannel(id: UUID): Server[(UUID, RequestResponseChannel)] =
+    Free.liftF(ServerChannel(id, createServerChannel(impl.dummyLogic)))
 
   sealed trait ConnectionAlg[T]
   case class ClientConnect(serverId: UUID) extends ConnectionAlg[RequestResponseChannel]
+  case class RegisterServer(serverId: UUID) extends ConnectionAlg[ServerChannel]
   case class ServerConnect(serverId: UUID, channel: RequestResponseChannel) extends ConnectionAlg[(UUID, RequestResponseChannel)]
   type Connection[T] = Free[ConnectionAlg, T]
   def createClientConnection(serverId: UUID): Connection[RequestResponseChannel] =
     Free.liftF(ClientConnect(serverId))
-  def registerServer(serverId: UUID, channel: RequestResponseChannel): Connection[(UUID, RequestResponseChannel)] =
+  def registerServer(): Connection[ServerChannel] =
+    Free.liftF(RegisterServer(UUID.randomUUID()))
+  def connectServer(serverId: UUID, channel: RequestResponseChannel): Connection[(UUID, RequestResponseChannel)] =
     Free.liftF(ServerConnect(serverId,channel))
 
   sealed trait ClientAlg[T]
@@ -56,9 +53,30 @@ object algebra {
 object interpreters {
   def serverToConnectionInterpreter: Translate[ServerAlg, Connection] = new (ServerAlg ~> Connection) {
     override def apply[A](f: ServerAlg[A]): Connection[A] = f match {
-      case RunServer(converter) =>
-        registerServer(UUID.randomUUID(), createServerChannel(converter))
+      case RunServer() =>
+        registerServer()
+      case ServerChannel(id, channel) =>
+        connectServer(id, channel)
     }
+  }
+
+  class AlgInterpreter[Alg[_]](implicit monad: Monad[({type f[a] = Free[Alg, a]})#f]) {
+    type FreeAlg[A] = Free[Alg,A]
+    def connectionToAlgInterpreter(serverConnections: Map[UUID, RequestResponseChannel] = Map())
+    : Translate[ConnectionAlg, FreeAlg] =
+      new (ConnectionAlg ~> FreeAlg) {
+        override def apply[A](f: ConnectionAlg[A]): FreeAlg[A] = f match {
+          case ClientConnect(serverId) =>
+            monad.unit(serverConnections(serverId))
+          case RegisterServer(serverId) =>
+            monad.unit(ServerChannel(serverId, createServerChannel(impl.dummyLogic)))
+          case ServerConnect(serverId, channel) =>
+            monad.unit(serverId -> channel)
+        }
+      }
+  }
+  object AlgInterpreter {
+    def apply[Alg[_]](implicit monad: Monad[({type f[a] = Free[Alg, a]})#f]): AlgInterpreter[Alg] = new AlgInterpreter[Alg]()
   }
 
   def clientToConnectionInterpreter: Translate[ClientAlg, Connection] = new (ClientAlg ~> Connection) {
@@ -67,42 +85,42 @@ object interpreters {
         createClientConnection(serverId)
     }
   }
-
-  def connectionToFutureInterpreter(serverConnections: Map[UUID, RequestResponseChannel] = Map())
-                                   (implicit ec: ExecutionContext): Translate[ConnectionAlg, Future] =
-    new (ConnectionAlg ~> Future) {
-      override def apply[A](f: ConnectionAlg[A]): Future[A] = f match {
-        case ClientConnect(serverId) =>
-          Future(serverConnections(serverId))
-        case ServerConnect(serverId, channel) =>
-          Future(serverId -> channel)
-      }
-    }
 }
 
 object compilers {
+  implicit val connFreeMonad: Monad[({type f[a] = Free[ConnectionAlg, a]})#f] =
+    freeMonad[ConnectionAlg]
+  implicit val serverFreeMonad: Monad[({type f[a] = Free[ServerAlg, a]})#f] =
+    freeMonad[ServerAlg]
+
   def runClientFree(serverId: UUID)
                    (implicit
-                    monad: Monad[Connection],
                     serverConnections: Map[UUID, RequestResponseChannel],
                     ec: ExecutionContext): Future[RequestResponseChannel] =
     runClient(serverId)
       .foldMap(clientToConnectionInterpreter)
-      .foldMap(connectionToFutureInterpreter(serverConnections))
+      .foldMap(AlgInterpreter[Future].connectionToAlgInterpreter(serverConnections))
+      .runFree
 
-  def runServerFree(converter: Converter[Request, Response])
-                   (implicit
-                    monad: Monad[Connection],
-                    ec: ExecutionContext): Future[(UUID, RequestResponseChannel)] =
-    runServer(converter)
+  def runServerFree()(implicit ec: ExecutionContext): Future[(UUID, RequestResponseChannel)] =
+    runServer()
       .foldMap(serverToConnectionInterpreter)
-      .foldMap(connectionToFutureInterpreter())
+      .foldMap(AlgInterpreter[ServerAlg].connectionToAlgInterpreter())
+      .foldMap(serverToConnectionInterpreter)
+      .foldMap(AlgInterpreter[Future].connectionToAlgInterpreter())
+      .runFree.map(t => t.id -> t.channel)
 
-  def makeRequest(request: Request, channel: RequestResponseChannel): Future[model.Response] =
+  def makeRequest(request: Request, channel: RequestResponseChannel): Future[model.Response] = // TODO interpreter
     emit(request)
       .through(channel)
       .runStreamProcess
       .map(_.head)
       .runFree
+}
+
+object impl {
+  val dummyLogic: Converter[Request, Response] = new Converter[Request, Response] {
+    def convert: Request => Response = req => Response(req.entity.map(ev => ev.copy(ts = ev.ts * 2)))
+  }
 }
 
