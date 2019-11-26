6ef0bdb63a8c52082591b8f6937f88eec33649d9
serbaev_ilnur <serbaevilnur@gmail.com>
2019-11-26 18:21:40 +0500

    anService - static html added

--- anService/src/main/scala/http.scala
+++ anService/src/main/scala/http.scala
@@ -1,3 +1,7 @@
+import java.io.File
+import java.nio.file.{Files, Paths}
+import java.util.concurrent.Executors
+
 import cats.effect._
 import org.http4s.{HttpRoutes, QueryParamDecoder}
 import org.http4s.syntax._
@@ -16,12 +20,33 @@ import org.http4s.circe._
 import org.http4s.dsl.io._
 import org.http4s.implicits._
 
+import scala.concurrent.ExecutionContext
+
 object http {
   private val logger = Logger("http")
   implicit private val decoder: EntityDecoder[IO, GetInfo] = jsonOf[IO, GetInfo]
+  private val blockingEC =
+    ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+  private val blockingCS: ContextShift[IO] =
+    IO.contextShift(blockingEC)
+  lazy val file = Files
+    .write(
+      Paths.get(new File("quiz.html").getAbsolutePath),
+      quiz.html.getBytes(java.nio.charset.Charset.forName("CP1251"))
+    )
+    .toFile
   private def route(implicit xa: Transactor[IO]) =
     HttpRoutes
       .of[IO] {
+        case request @ GET -> Root / "quiz.html" =>
+          implicit val cs = blockingCS
+          StaticFile
+            .fromFile(
+              file,
+              blockingEC,
+              Some(request)
+            )
+            .getOrElseF(NotFound())
         case req @ POST ->
             Root / "records" / "body" / "mail" / mail =>
           for {
